c56e93f9c8eb903fccb66c690a57eaf510617fb6
Jett <ma.jiangtao.86@gmail.com>
2018-06-09 09:18:38 +0800

    Add: get frame image at sepecified time from the video url

--- PlayerKit/PlayerKit.m
+++ PlayerKit/PlayerKit.m
@@ -29,6 +29,36 @@
     return PlayerFileManager.sharedInstance.cacheDirectory;
 }
 
++ (void)getFrameWithVideoURL:(NSURL *)url
+                      atTime:(NSTimeInterval)seconds
+                  completion:(void (^)(UIImage * _Nullable image))completion {
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:url options:nil];
+        NSParameterAssert(asset);
+        AVAssetImageGenerator *generator = [[AVAssetImageGenerator alloc] initWithAsset:asset];
+        generator.appliesPreferredTrackTransform = YES;
+        generator.apertureMode = AVAssetImageGeneratorApertureModeEncodedPixels;
+        generator.requestedTimeToleranceAfter = kCMTimeZero;
+        generator.requestedTimeToleranceBefore = kCMTimeZero;
+        
+        CGImageRef imageRef = NULL;
+        NSError *error = nil;
+        CMTime time = CMTimeMakeWithSeconds(seconds, 1000);
+        CMTime actualTime;
+        imageRef = [generator copyCGImageAtTime:time
+                                     actualTime:&actualTime error:&error];
+        if(!imageRef) {
+            NSLog(@"%@ error %@", NSStringFromSelector(_cmd), error);
+            completion(nil);
+        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIImage *image = [[UIImage alloc] initWithCGImage:imageRef];
+            CGImageRelease(imageRef);
+            completion(image);
+        });
+    });
+}
+
 - (void)dealloc {
     [PlayerFileManager.sharedInstance clean];
 }
