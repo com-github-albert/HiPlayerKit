d270b70c3859d7091ac2628e8932c275f9cad01e
Jett <ma.jiangtao.86@gmail.com>
2018-06-16 11:21:11 +0800

    Update: podspec

--- PlayerKit/PlayerKit.m
+++ PlayerKit/PlayerKit.m
@@ -11,7 +11,7 @@
 
 @implementation PlayerKit
 
-+ (NSString *)version { return @"0.1.0"; }
++ (NSString *)version { return @"0.0.1"; }
 
 + (void)setDestDirectory:(NSString *)destDirectory {
     PlayerFileManager.sharedInstance.destDirectory = destDirectory;
